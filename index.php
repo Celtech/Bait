94d26ff96326b2a91da39dd9228e95a9e4b13c57
Tim Hinz <timhinz16@gmail.com>
2017-06-28 15:04:01 -0500

    Base project addition.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bait | Bugs, analytics, issues, and to-do's tracker</title>
+        <meta name="description" content="Bait is the goto full featured bugs, analytics, issues, and to-do's tracker." />
+        <meta name="keywords" content="Bugs, analytics, Issues, to-dos, tracker, bug tracker, issue tracker, collaborate, team build, git issues, git, git bug, bug, todo, issue" />
+        <meta name="author" content="metatags generator">
+        <meta name="robots" content="index, follow">
+        <meta name="revisit-after" content="3 month">
+
+        <link rel="shortcut icon" href="imgs/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="css/main.min.css">
+        <script src="js/jquery-3.2.1.min.js"></script>
+    </head>
+
+    <body>
+
+    </body>
+</html>
